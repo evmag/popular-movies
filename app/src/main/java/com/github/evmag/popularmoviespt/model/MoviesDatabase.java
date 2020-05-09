63241b6ac94d43765ed06a3fee98eac58a983768
evmag <vag.mg4@gmail.com>
2020-05-09 12:12:07 +0200

    - Add movies database

---
+++ app/src/main/java/com/github/evmag/popularmoviespt/model/MoviesDatabase.java
@@ -0,0 +1,42 @@
+package com.github.evmag.popularmoviespt.model;
+
+import android.content.Context;
+
+import androidx.room.Database;
+import androidx.room.Room;
+import androidx.room.RoomDatabase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Database(entities = {Movie.class, Trailer.class, Review.class}, version = 1, exportSchema = false)
+public abstract class MoviesDatabase extends RoomDatabase {
+    public static final String POPULAR_MOVIES_DB_NAME = "popular_movies";
+    public static final String TOP_RATED_MOVIES_DB_NAME = "top_rated_movies";
+    public static final String FAVORITE_MOVIES_DB_NAME = "favorite_movies";
+
+    private static final Object LOCK = new Object();
+
+    private static Map<String, MoviesDatabase> sMoviesDBs;
+
+    public static MoviesDatabase getInstance(Context context, String dbName) {
+        if (sMoviesDBs == null) {
+            sMoviesDBs = new HashMap<>();
+        }
+
+        MoviesDatabase moviesDatabase= sMoviesDBs.get(dbName);
+        if (moviesDatabase == null) {
+            synchronized (LOCK) {
+                moviesDatabase = Room.databaseBuilder(context.getApplicationContext(),
+                        MoviesDatabase.class,
+                        dbName)
+                        .build();
+                sMoviesDBs.put(dbName, moviesDatabase);
+            }
+        }
+
+        return moviesDatabase;
+    }
+
+    public abstract MoviesDao moviesDao();
+}
