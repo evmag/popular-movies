44f5c939160e2c02fddfea36d6c66622a2205969
evmag <vag.mg4@gmail.com>
2020-05-09 11:52:08 +0200

    - Add movies DAO

---
+++ app/src/main/java/com/github/evmag/popularmoviespt/model/MoviesDao.java
@@ -0,0 +1,29 @@
+package com.github.evmag.popularmoviespt.model;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface MoviesDao {
+    @Query("SELECT * FROM movies")
+    List<Movie> getMovies();
+
+    @Query("SELECT * FROM movies WHERE movie_id = :movieId")
+    Movie getMovieById(int movieId);
+
+    @Delete
+    void deleteMovie(Movie movie);
+
+    @Insert
+    void insertMovie(Movie movie);
+
+    @Insert
+    void insertAllMovies(List<Movie> movies);
+
+    @Query("DELETE FROM movies")
+    void deleteAllMovies();
+}
